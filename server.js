c395c05c3899fae03c227ef1bddab84844c71f22
adarsh kv <adarshkv224@gmail.com>
2025-09-27 07:03:24 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require('express')
+require('dotenv').config()
+const app=express()
+const PORT=process.env.PORT
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on ",PORT);
+    
+})
